0ef9e48f17179fbfd40c862e62f2a7b72887294d
PARHELIA 3 <parhelia3@iMac-de-PARHELIA-2.local>
2018-06-04 18:23:55 +0200

    Creation de la vue formulaire et modification du controller

--- src/DrDoh/TicketBundle/Controller/DefaultController.php
+++ src/DrDoh/TicketBundle/Controller/DefaultController.php
@@ -3,12 +3,19 @@
 namespace DrDoh\TicketBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use DrDoh\TicketBundle\Entity\Ticket;
+use DrDoh\TicketBundle\Form\TicketType;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('DrDohTicketBundle:Default:index.html.twig');
+        $ticket = new Ticket();
+        $form = $this->get('form.factory')->create(TicketType::class, $ticket);
+        return $this->render('DrDohTicketBundle:Default:index.html.twig',array(
+            'form' => $form->createView(),
+        ));
     }
 
 }
